a0fa9fe5ec252f55198c68c3258609dad998ec45
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-10-01 22:55:17 -0400

    updated imports for app.jsx which did not have apolloClient imports.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import './App.css';
+// Bring in the Outlet component which will render the proper pages conditionally on the browser's URL
+import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Navbar />
+      <Outlet />
+    </ApolloProvider>
   );
 }
 
