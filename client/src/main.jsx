a7ccde6b05d2e43b93de9ac67e971b9e0336cdc0
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-10-02 03:27:15 -0400

    updated code in server.js file. configured dotenv as well as middleware connections for apollo.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// Import the required Provider component and createBrowserRouter helper function
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Import the pages the application should be able to display
+import App from './App.jsx';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+
+// Define the router object which will control the Provider's ability to display certain pages to match the proper URLs
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />,
+      },
+      {
+        path: '/saved',
+        element: <SavedBooks />,
+      },
+    ],
+  },
+]);
+
+// Render the Provider which will keep track of the application's location
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 );
